c77c0f4d1b9bd86dcfaa2ab8fed820d64a121eaf
Tonnarrrm <anuwatch6552@gmail.com>
2022-12-09 15:48:31 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int i = 1;
+    int B = 0;
+    int C = 0;
+    while(i!=0){
     cout << "Enter an integer: ";
+    cin >> i;
+    if (i%2 != 0){
+        C++;
+    }
+    else {
+        B++;
+    }
+    }
+
     cout << "#Even numbers = ";
+    cout << B-1 << "\n";
     cout << "#Odd numbers = ";
+    cout << C;
     return 0;
 }
